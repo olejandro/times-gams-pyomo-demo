0d1ce70acc9e1ba2d288b1ee40c500c26248b806
Olexandr@MS <obal@dtu.dk>
2018-06-16 15:13:25 +0200

    Rolled back to a previous version of TIMES and adjusted paths in run files

--- DEMO_pyGLPKrun.cmd
+++ DEMO_pyGLPKrun.cmd
@@ -1,12 +1,12 @@
 SET CASE=UTOPIA
 IF NOT %1.==. SET CASE=%1
 IF %1.==0. SET CASE=UTOPIA
-CALL GAMS %CASE%.RUN IDIR=..\TIMES PS=0 GDX=%CASE% GDXCOMPRESS=1 --EXTSOL=YES
+CALL GAMS %CASE%.RUN IDIR=TIMES PS=0 GDX=%CASE% GDXCOMPRESS=1 --EXTSOL=YES
 IF %1.==0. PAUSE
-..\GAMS\gdx2sqlite -i %CASE%.gdx -o PROTO.db3 -small -fast
+GAMS\gdx2sqlite -i %CASE%.gdx -o PROTO.db3 -small -fast
 IF %1.==0. PAUSE
 pyomo solve --solver=glpk --solver-executable=GLPK/glpsol --symbolic-solver-labels --stream-solver --report-timing --tempdir=%cd% tipyomo.py loadall.dat
 PAUSE
-CALL GAMS %CASE%.RUN IDIR=..\TIMES PS=0 GDX=%CASE% GDXCOMPRESS=1 --EXTSOL=yes
-..\GAMS\GDX2VEDA %CASE% ..\times\times2veda.vdd %CASE%-PYOMO
+CALL GAMS %CASE%.RUN IDIR=TIMES PS=0 GDX=%CASE% GDXCOMPRESS=1 --EXTSOL=yes
+GAMS\GDX2VEDA %CASE% times\times2veda.vdd %CASE%-PYOMO
 PAUSE
