73b53e5fcf818ccd1a5ae69ff636449ac618a6fd
olejandro <olex@energymodelling.club>
2024-10-13 11:46:55 -0500

    Move all execution to gamsrun.py

--- gamsrun.py
+++ gamsrun.py
@@ -3,11 +3,13 @@ from importlib import resources
 
 case = "UTOPIA"
 
-# Path to gams.exe
+# Paths to resources
 gams_path = resources.files("gamspy_base") / "gams.exe"
+gdx2veda_path = resources.files("gamsrun") / "GAMS" / "GDX2VEDA.exe"
+times2veda_path = resources.files("gamsrun") / "source" / "times2veda.vdd"
 
 # Command to execute gams.exe
-command = [
+gams_command = [
     gams_path,
     f"{case}.RUN",
     "IDIR=source",
@@ -17,5 +19,14 @@ command = [
     f"O={case}-GAMS.LST",
 ]
 
-# Execute gams.exe
-subprocess.run(command)
+# Command to execute gdx2veda.exe
+gdx2veda_command = [
+    gdx2veda_path,
+    f"{case}-GAMS",
+    times2veda_path,
+    f"{case}-GAMS",
+]
+
+# Execute subprocesses
+subprocess.run(gams_command)
+subprocess.run(gdx2veda_command)
