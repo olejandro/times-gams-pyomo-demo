56f5ec41ba804112f87266e64c4acd19ee473dd9
olejandro <olex@energymodelling.club>
2025-04-26 13:22:26 +0300

    Simplify gamsrun.py

--- gamsrun.py
+++ gamsrun.py
@@ -1,32 +1,34 @@
 import subprocess
-from importlib import resources
+from pathlib import Path
+from gamspy_base import directory
 
 case = "UTOPIA"
 
 # Paths to resources
-gams_path = resources.files("gamspy_base") / "gams.exe"
-gdx2veda_path = resources.files("gamsrun") / "GAMS" / "GDX2VEDA.exe"
-times2veda_path = resources.files("gamsrun") / "source" / "times2veda.vdd"
+gams_path = Path(directory) / "gams.exe"
+gdx2veda_path = Path("GAMS") / "GDX2VEDA.exe"
+times2veda_path = Path("source") / "times2veda.vdd"
 
-# Command to execute gams.exe
-gams_command = [
-    gams_path,
-    f"{case}.RUN",
-    "IDIR=source",
-    "PS=0",
-    f"GDX={case}-GAMS",
-    "GDXCOMPRESS=1",
-    f"O={case}-GAMS.LST",
-]
-
-# Command to execute gdx2veda.exe
-gdx2veda_command = [
-    gdx2veda_path,
-    f"{case}-GAMS",
-    times2veda_path,
-    f"{case}-GAMS",
-]
+commands = {
+    "GAMS": [
+        gams_path,
+        f"{case}.RUN",
+        "IDIR=source",
+        "PS=0",
+        f"GDX={case}-GAMS",
+        "GDXCOMPRESS=1",
+        f"O={case}-GAMS.LST",
+        ],
+    "gdx2veda": [
+        gdx2veda_path,
+        f"{case}-GAMS",
+        times2veda_path,
+        f"{case}-GAMS",
+        ]
+}
 
 # Execute subprocesses
-subprocess.run(gams_command)
-subprocess.run(gdx2veda_command)
+for name, command in commands.items():
+    print(f"Executing {name}...")
+    subprocess.run(command)
+
